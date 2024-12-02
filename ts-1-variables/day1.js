a6b4bb96d64dc3199d0367bc3778953989fed32f
Sashi Vattikuti <sashi.kumar40@gmail.com>
2024-12-02 16:55:15 +0000

    Exercise-1

--- ts-1-variables/day1.js
+++ ts-1-variables/day1.js
@@ -9,4 +9,11 @@
     var deposit = Number(prompt('Deposit Amount'));
     mBalance += +deposit;
     console.log('Your new balance is:', mBalance);
+    var nBalance = 'Your new balance is:';
+    var zeroBalance = true;
+    console.log(typeof nBalance, typeof mBalance, typeof zeroBalance);
+    console.log(nBalance, mBalance, zeroBalance = mBalance === 0);
+    var greeting = 'Hello';
+    var clientName = String(prompt("Enter your Name"));
+    console.log(greeting, clientName, nBalance, mBalance);
 }
--- ts-1-variables/day1.ts
+++ ts-1-variables/day1.ts
@@ -14,5 +14,17 @@
     mBalance += +deposit
 
     console.log('Your new balance is:', mBalance)
+
+    let nBalance:string = 'Your new balance is:'
+    let zeroBalance:boolean = true
+
+    console.log(typeof nBalance, typeof mBalance, typeof zeroBalance)
+    console.log(nBalance, mBalance, zeroBalance = mBalance===0)
+
+    let greeting:string = 'Hello'
+    let clientName:string = String(prompt("Enter your Name"))
+
+    console.log(greeting, clientName, nBalance, mBalance)
+
 }
 
