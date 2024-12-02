3063cde9e523d0ecf1b03c3e506aec028e818c37
Sashi Vattikuti <sashi.kumar40@gmail.com>
2024-12-02 16:13:38 +0000

    Java script practice Day1

--- js-1/day1.js
+++ js-1/day1.js
@@ -1,2 +1,17 @@
 console.log("Hello Tampa!!")
-alert("WARNING!!!")
+//alert("WARNING!!!")
+
+const balance = 1000
+console.log(typeof balance)
+
+var mBalance = 1000
+mBalance = 20000
+
+console.log(mBalance)
+
+let deposit = prompt('Deposit Amount')
+mBalance += +deposit
+
+console.log('Your new balance is:', mBalance)
+
+
